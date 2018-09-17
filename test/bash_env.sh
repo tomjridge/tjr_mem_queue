d0b0e30de7fdacc777b2c2b38f20767adddf1b73
Tom Ridge <tom.j.ridge@googlemail.com>
2018-09-17 19:59:58 +0100

    +

--- test/bash_env.sh
+++ test/bash_env.sh
@@ -5,7 +5,7 @@ PKGS="-package tjr_mem_queue,tjr_mem_queue_lwt,lwt.unix -linkpkg"
 
 # see https://caml.inria.fr/pub/docs/manual-ocaml/comp.html
 # WARN="-w @f@p@u@s@40-8-11-26-40-20"
-WARN=""
+WARN="-w -26"
 
   ocamlc="$DISABLE_BYTE ocamlfind ocamlc   -g -bin-annot $WARN $PKGS"
 ocamlopt="$DISABLE_NTVE ocamlfind ocamlopt -g -bin-annot $WARN $PKGS"
