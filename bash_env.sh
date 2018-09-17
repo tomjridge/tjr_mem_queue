04cbc69daaf7ed6cff3a267cf1631a33b20856fb
Tom Ridge <tom.j.ridge@googlemail.com>
2018-09-17 13:59:09 +0100

    +

---
+++ bash_env.sh
@@ -0,0 +1,112 @@
+set -a # export all vars
+# set -x # debug
+
+libname=tjr_mem_queue
+Libname=Tjr_mem_queue
+
+
+meta_description="tjr_mem_queue, an in-mem message queue for OCaml threads"
+
+# required_packages="stdlib" 
+
+# FIXME a bit inefficient if recalculating every time
+# mls=`ocamlfind ocamldep -package $required_packages -sort -one-line *.ml`
+mls=tjr_mem_queue.ml
+
+natives=""
+bytes=""
+
+
+# common ---------------------------------------------------------------
+
+
+
+# set these env vars before including the file
+function check_env_vars () {
+    # http://stackoverflow.com/questions/31164284/shell-script-exiting-script-if-variable-is-null-or-empty
+    : ${libname?Need a value}
+    : ${Libname?Need a value}
+    : ${mls?Need a value}
+    : ${meta_description?Need a value}
+#    : ${required_packages?Need a value}
+}
+check_env_vars
+
+# PKGS="-package $required_packages"
+PKGS="-I +threads"
+
+
+# see https://caml.inria.fr/pub/docs/manual-ocaml/comp.html
+# WARN="-w @f@p@u@s@40-8-11-26-40-20"
+WARN=""
+
+  ocamlc="$DISABLE_BYTE ocamlfind ocamlc   -g -bin-annot $WARN $PKGS"
+ocamlopt="$DISABLE_NTVE ocamlfind ocamlopt -g -bin-annot $WARN $PKGS"
+ocamldep="ocamlfind ocamldep $PKGS"
+
+
+# mls ----------------------------------------
+
+cmos="${mls//.ml/.cmo}"
+cmxs="${mls//.ml/.cmx}"
+
+
+# cma,cmxa -------------------------------------------------------------
+
+function mk_cma() {
+  $DISABLE_BYTE ocamlfind ocamlc -g -a -o $libname.cma $cmos
+}
+
+function mk_cmxa() {
+  $DISABLE_NTVE ocamlfind ocamlopt -g -a -o $libname.cmxa $cmxs
+}
+
+
+
+
+# meta ----------------------------------------
+
+function mk_meta() {
+cat >META <<EOF
+name="$libname"
+description="$meta_description"
+version="??"
+requires="$required_packages"
+archive(byte)="$libname.cma"
+archive(native)="$libname.cmxa"
+EOF
+
+}
+
+
+
+# doc ----------------------------------------------------
+
+function mk_doc() {
+    ocamlfind ocamldoc $PKGS $WARN -html -intro intro.odoc *.ml
+    mv *.html ../docs/ocamldoc
+    rm *.css
+# odoc -----------------------------------------------------------------
+# depends on jbuilder really; the following isn't quite right
+#    for f in *.cmt; do odoc compile --package $libname $f; done
+#    for f in *.odoc; do odoc html -o /tmp $f; done
+}
+
+
+# clean ----------------------------------------------------------------
+
+function clean() {
+	rm -f *.{cmi,cmo,cmx,o,cmt} a.out *.cma *.cmxa *.a *.byte *.native *.odoc *.html *.css
+}
+
+# ocamlfind install, remove, reinstall --------------------
+
+function install() {
+    # assumes packing
+	  ocamlfind install $libname META $libname.{cmi,cmo,cma,cmx,cmxa,a} *.ml
+    # FIXME how to install cmt file for libname?
+}
+
+function remove() {
+    ocamlfind remove $libname
+}
---
+++ tjr_mem_queue.ml
@@ -0,0 +1,30 @@
+type 'a queue = {
+  q: 'a Queue.t;
+  mutex: Mutex.t;
+  cvar: Condition.t
+}
+           
+
+let enqueue ~msg ~q =
+  Mutex.lock q.mutex;
+  Queue.add msg q.q;
+  Condition.signal q.cvar;
+  Mutex.unlock q.mutex
+
+
+let dequeue ~q =
+  Mutex.lock q.mutex;
+  while (Queue.is_empty q.q) do
+    Condition.wait q.cvar q.mutex
+  done;
+  let msg = Queue.take in
+  Mutex.unlock q.mutex;
+  msg
+
+
+let create () = {
+  q=Queue.create();
+  mutex=Mutex.create ();
+  cvar=Condition.create ()
+}
+
